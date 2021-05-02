2466919ca25f02930c5a02876cfa409fb190459c
Rishabh <baidrishabh443@gmail.com>
2021-05-02 16:11:19 +0530

    made

---
+++ build/precache-manifest.f63844628fc99058542871f5dc9325d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e416c9ca76a2791b53017bbc894b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b506c3f02b8883cebd8c",
+    "url": "/static/css/main.e28533b4.chunk.css"
+  },
+  {
+    "revision": "3b768eeabc427c477544",
+    "url": "/static/js/2.7bd8695e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.7bd8695e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b506c3f02b8883cebd8c",
+    "url": "/static/js/main.851f388f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
